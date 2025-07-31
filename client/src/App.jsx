2c51ce6a6f5cd8f8ca9fa6d70f96f0d38eee24eb
Muli Muthui <147803860+2muli@users.noreply.github.com>
2025-08-01 01:35:40 +0300

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import { Route, Routes } from "react-router-dom";
-import "./App.css";
 
 import ProtectedRoute from "./context/ProtectedRoute";
 
